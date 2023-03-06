a541d5c20ff50f469e9f6a659769d676500de3dc
Nicolaiski <ngaudu@gmail.com>
2023-03-06 14:53:36 -0500

    app

--- src/main.jsx
+++ src/main.jsx
@@ -24,15 +24,15 @@ root.render(
   <React.StrictMode>
     <HashRouter>
     {/* <BrowserRouter> */}
-      <Routes>
-        {/* <Route path="*" element={<App/>} /> */}
+      {/* <Routes>
+         <Route path="*" element={<App/>} /> 
         <Route path="/" element={<App/>} /> 
-        {/* <Route path="/" element={<HomePage />} /> */}
-        {/* <Route path="/UserPage" element={<UserPage />} />
+         <Route path="/" element={<HomePage />} /> 
+         <Route path="/UserPage" element={<UserPage />} />
         <Route path="/Music" element={<Music />} />
-        <Route path="/UserPageDesigns" element={<UserPageDesigns />} /> */}
-      </Routes>
-      
+        <Route path="/UserPageDesigns" element={<UserPageDesigns />} /> 
+      </Routes> */}
+       <App />
     </HashRouter>
     {/* </BrowserRouter> */}
   </React.StrictMode>
