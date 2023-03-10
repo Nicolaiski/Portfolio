f2a4120d577cfb99d7ea4691fae3723184a9c22a
Nicolaiski <ngaudu@gmail.com>
2023-03-10 15:12:36 -0500

    mltest10

--- src/Pages/UserPageDesigns.jsx
+++ src/Pages/UserPageDesigns.jsx
@@ -440,7 +440,7 @@ export default function UserPageDesigns() {
 
 
                 <div>
-                  <div className=" xl:ml-[350px] mt-[250px] mb-[300px] w-[1200px] group">
+                  <div className=" xl:ml-[350px] mt-[0px] mb-[300px] w-[1200px] group">
                   <button className=" mt-[400px] group-hover:my-rotate-y-180 text-center ml-[390px] items-center 
                         bg-gradient my-8 font-poppins text-gradient text-[128px] font-bold
                         - delay-100 duration-500 rounded-full"> 
